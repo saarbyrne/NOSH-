325b0038702cc75ba50c25a81fdada2b4630d2e2
Saar <saar@kitmanlabs.com>
2025-07-22 19:13:08 +0200

    app studio

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -7,13 +7,12 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../sanity.config'
+import StudioWrapper from './studio-wrapper'
 
-export const dynamic = 'force-static'
+export const dynamic = 'force-dynamic'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <StudioWrapper />
 }
